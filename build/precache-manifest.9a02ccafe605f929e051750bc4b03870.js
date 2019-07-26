07044f417c830e57da510d5ddabf9be98fe0e5ab
Felipe de Alcântara Monteiro <36462206+SquallxX@users.noreply.github.com>
2019-07-26 13:41:22 -0300

    Add files via upload

---
+++ build/precache-manifest.9a02ccafe605f929e051750bc4b03870.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d92b55babf879a3039226c2ac7182231",
+    "url": "/venturus_frontend_test_2/index.html"
+  },
+  {
+    "revision": "a5cd171ca027f102fea0",
+    "url": "/venturus_frontend_test_2/static/css/2.4aa6a59b.chunk.css"
+  },
+  {
+    "revision": "fb03eb071502f35c9356",
+    "url": "/venturus_frontend_test_2/static/css/main.4b330a82.chunk.css"
+  },
+  {
+    "revision": "a5cd171ca027f102fea0",
+    "url": "/venturus_frontend_test_2/static/js/2.12da78ec.chunk.js"
+  },
+  {
+    "revision": "fb03eb071502f35c9356",
+    "url": "/venturus_frontend_test_2/static/js/main.90e40494.chunk.js"
+  },
+  {
+    "revision": "53c09d33a2ff669edf67",
+    "url": "/venturus_frontend_test_2/static/js/runtime~main.c933b6b5.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/venturus_frontend_test_2/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/venturus_frontend_test_2/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/venturus_frontend_test_2/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/venturus_frontend_test_2/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/venturus_frontend_test_2/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
